98abf488d0d28a81bbf3ec17dd4ef41ba8ab654f
MelHellrigl <hellriglandreamelissa@gmail.com>
2022-11-03 16:37:50 -0300

    modificaciones

--- api/src/db.js
+++ api/src/db.js
@@ -28,7 +28,7 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henry-food-mh`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
         logging: false,
         native: false,
       });
