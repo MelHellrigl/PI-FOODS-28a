8b5f7372f346d923565e815aed75ee7fcbcdf6b9
MelHellrigl <hellriglandreamelissa@gmail.com>
2022-11-03 17:39:58 -0300

    modificaciones

--- api/src/app.js
+++ api/src/app.js
@@ -8,10 +8,15 @@ require("./db.js");
 
 const server = express();
 const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 server.name = "API";
 
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
